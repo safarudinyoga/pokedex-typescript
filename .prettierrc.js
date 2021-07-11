4e187b8262c7a2eb173276440d122268f307ffae
safarudinyoga <safarudinyoga@gmail.com>
2021-07-11 18:09:29 +0700

    initial

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "none",
+  bracketSpacing: true,
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always'
+}
